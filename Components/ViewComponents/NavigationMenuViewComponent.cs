65ab7f8d5d81aca7773f8d773a46c3da8c3c5f47
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-06-01 15:36:53 -0400

    added migration to azure DB and CRUD functionalities

---
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Churn.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+
+namespace WorldDominion.Components.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<MenuItem>
+            {
+                    new MenuItem {Controller = "Home", Action = "Index", Label = "Home"},
+                    new MenuItem {Controller = "Home", Action = "About", Label = "About"},
+                    new MenuItem {Controller = "Categories", Action = "Index", Label = "Categories", DropdownItems = new List<MenuItem> {
+                    new MenuItem {Controller = "Categories", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Categories", Action = "Create", Label = "Create"},
+                    } },
+                    new MenuItem {Controller = "Products", Action = "Index", Label = "Products", DropdownItems = new List<MenuItem> {
+                    new MenuItem {Controller = "Products", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Products", Action = "Create", Label = "Create"},
+                    } },
+                    new MenuItem {Controller = "Home", Action = "Privacy", Label = "Privacy"}
+
+            };
+            return View(menuItems); //becomes model in the view
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Churn.Models;
 
 namespace Churn.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        
+        public DbSet<Churn.Models.Category>? Category { get; set; }
+        public DbSet<Churn.Models.Product>? Product { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
     }
-}
+} 
