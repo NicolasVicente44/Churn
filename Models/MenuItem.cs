4e82939e7212404722571c8b96fd78a1d8505f02
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-06-24 13:24:31 -0400

    Modified model views

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -5,6 +5,9 @@
         public string Controller { get; set; }
         public string Action { get; set; }
         public string Label { get; set; }
+
+        public object RouteValues { get; set; } 
+
         public List<MenuItem> DropdownItems { get; set; }
     }
 }
