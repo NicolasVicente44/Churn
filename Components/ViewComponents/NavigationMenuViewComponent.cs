15ad3e3c6c4371f7f7115e0f9d964414358ef326
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-06-25 13:08:22 -0400

    Modfied model and home views

--- Components/ViewComponents/NavigationMenuViewComponent.cs
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -16,23 +16,23 @@ namespace WorldDominion.Components.ViewComponents
                     new MenuItem {Controller = "Home", Action = "Privacy", Label = "Privacy"},
 
                     new MenuItem {Controller = "CreditCards", Action = "Index", Label = "Credit Cards", DropdownItems = new List<MenuItem> {
-                    new MenuItem {Controller = "CreditCards", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "CreditCards", Action = "Index", Label = "All Credit Cards"},
                     new MenuItem {Controller = "CreditCards", Action = "Create", Label = "Create"},
                     } },
                     new MenuItem {Controller = "Mortgages", Action = "Index", Label = "Mortgages", DropdownItems = new List<MenuItem> {
-                    new MenuItem {Controller = "Mortgages", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Mortgages", Action = "Index", Label = "All Mortgages"},
                     new MenuItem {Controller = "Mortgages", Action = "Create", Label = "Create"},
                     } },
                     new MenuItem {Controller = "Loans", Action = "Index", Label = "Loans", DropdownItems = new List<MenuItem> {
-                    new MenuItem {Controller = "Loans", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Loans", Action = "Index", Label = "All Loans"},
                     new MenuItem {Controller = "Loans", Action = "Create", Label = "Create"},
                     } },
                     new MenuItem {Controller = "Accounts", Action = "Index", Label = "Accounts", DropdownItems = new List<MenuItem> {
-                    new MenuItem {Controller = "Accounts", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Accounts", Action = "Index", Label = "All Accounts"},
                     new MenuItem {Controller = "Accounts", Action = "Create", Label = "Create"},
                     } }, 
                     new MenuItem {Controller = "Investments", Action = "Index", Label = "Investments", DropdownItems = new List<MenuItem> {
-                    new MenuItem {Controller = "Investments", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Investments", Action = "Index", Label = "All Investments"},
                     new MenuItem {Controller = "Investments", Action = "Create", Label = "Create"},
                     } }, 
 
